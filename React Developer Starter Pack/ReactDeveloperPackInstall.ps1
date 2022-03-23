36293f9f21e71318fb48972fe8e914a2416c1e57
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2022-03-23 19:06:03 -0300

    Adicionando o react developer pack e ajustando o uninstall de python developer pack

---
+++ React Developer Starter Pack/ReactDeveloperPackInstall.ps1
@@ -0,0 +1,9 @@
+winget install --id=OpenJS.NodeJS;
+winget install --id=Docker.DockerDesktop;
+winget install --id=Opera.OperaGX;
+winget install --id=Google.Chrome;
+winget install --id=Mozilla.Firefox
+winget install --id=Git.Git;
+winget install --id=Microsoft.VisualStudioCode;
+winget install --id=Postman.Postman;
+./VSCodeExtensions.ps1
