6bdc2d6e7cdbcec48c735f1ec6ad6f593c4d5dc7
Joseph Alexandre <j.alexandremmfreitas@gmail.com>
2022-03-22 21:29:13 -0300

    Scripts do Python Developer Starter Pack

---
+++ Python Developer Starter Pack/PythonDeveloperPackInstall.ps1
@@ -0,0 +1,9 @@
+winget install --id=Opera.OperaGX;
+winget install --id=Google.Chrome;
+winget install --id=Git.Git;
+winget install --id=Python.Python.3;
+winget install --id=Microsoft.VisualStudioCode;
+winget install --id=Postman.Postman;
+winget install --id=SublimeHQ.SublimeMerge;
+winget install --id=Docker.DockerDesktop;
+./VSCodeExtensions.ps1
